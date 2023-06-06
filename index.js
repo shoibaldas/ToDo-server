02c1c2025219746f92e212826a8a887e97727217
Shoibal Das <your_email@example.com>
2023-06-06 09:15:37 +0600

    mongo connection fixed

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vrrg6hy.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
 // const client = new MongoClient(uri);
 
 async function dbConnect() {
